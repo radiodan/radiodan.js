a046746a3f4081ab14b6164e472ebae8fbf0e316
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2014-01-13 17:11:56 +0000

    spawning a child process

--- test/test-mpd-child-process.js
+++ test/test-mpd-child-process.js
@@ -1,21 +1,69 @@
-var assert = require("assert"),
+var chai = require("chai"),
+    assert = chai.assert,
+    chaiAsPromised = require("chai-as-promised"),
     sinon  = require("sinon"),
     EventEmitter = require("events").EventEmitter,
     net    = require("net");
 
-var subject = require("../lib/mpd-process");
+var Q = require('Q');
+
+chai.use(chaiAsPromised);
+
+var subject = require("../lib/mpd-child-process");
 
 describe('mpdProcess', function(){
 
   describe('locating the mpd binary', function () {
-    it('finds the first mpd binary on the system');
-    it('rejects the promise if no mpd binary is found');
+    it('uses which to locate binary', function () {
+      var exec = sinon.stub().returns({ done: function () {} });
+      subject.processPath(exec);
+      assert(exec.calledWith('which mpd'), 'wrong command');
+    });
+
+    it('finds the first mpd binary on the system', function (done) {
+
+      var exec = function () {
+        var dfd = Q.defer(),
+            promise = dfd.promise;
+
+        dfd.resolve(['/sommat/sommert/mpd']);
+        return promise;
+      }
+
+      subject.processPath(exec);
+
+      assert.becomes(subject.processPath(exec), '/sommat/sommert/mpd')
+            .notify(done);
+    });
   });
 
   describe('spawning a child mpd process', function () {
-    it('spawns using the correct binary');
-    it('fulfills the promise on successful spawning');
+
+    before(function () {
+      this.binaryPath = '/sommat/mpd';
+    });
+
+    it('spawns using the correct binary', function (done) {
+      var mock = sinon.spy(),
+          binPath = this.binaryPath;
+
+      subject.processPath = function () {
+        var dfd = Q.defer();
+        dfd.resolve(binPath);
+        return dfd.promise;
+      };
+
+      var promise = subject.create('some/config.conf', mock);
+
+      promise.then(function () {
+        assert(mock.calledWithExactly(binPath, ['some/config.conf', '--no-daemon']));
+        assert(promise.isFulfilled());
+        done();
+      });
+    });
+
     it('rejects the promise on spawning error');
+    it('rejects the promise if no mpd binary is found');
   });
 
   describe('logging output from the mpd process', function () {
