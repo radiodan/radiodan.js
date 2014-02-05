6037c508821686b917222d412642013f9a786891
Dan Nuttall <pixelblend@gmail.com>
2014-02-05 14:36:06 +0000

    Cleanup test

--- test/lib/test-utils.js
+++ test/lib/test-utils.js
@@ -5,9 +5,7 @@ var chai = require('chai'),
     assert = chai.assert,
     chaiAsPromised = require('chai-as-promised'),
     sinon  = require('sinon'),
-    winston = require('winston'),
-    fs     = require('fs'),
-    EventEmitter = require('events').EventEmitter;
+    winston = require('winston');
 
 var Q = require('q');
 
