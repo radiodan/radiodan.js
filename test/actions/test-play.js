e2ba407354ffea858f9b628b1131ae40f916658a
Dan Nuttall <pixelblend@gmail.com>
2014-02-06 11:30:28 +0000

    Set play position in playlist

--- test/actions/test-play.js
+++ test/actions/test-play.js
@@ -4,12 +4,7 @@
 var chai = require('chai'),
     assert = chai.assert,
     chaiAsPromised = require('chai-as-promised'),
-    sinon  = require('sinon'),
-    winston = require('winston'),
-    fs     = require('fs'),
-    EventEmitter = require('events').EventEmitter;
-
-var utils = require('../../lib/utils');
+    sinon  = require('sinon');
 
 chai.use(chaiAsPromised);
 
@@ -22,7 +17,7 @@ describe('play action', function() {
     subject(radio, {playlist: ['track.mp3'], playNow: false});
     assert.equal(1, radio.sendCommands.callCount);
     assert.ok(radio.sendCommands.calledWith([
-        ['clear'], ['add', 'track.mp3'], ['random', '0']
+      ['clear'], ['add', 'track.mp3'], ['random', '0']
     ]));
   });
 
@@ -34,6 +29,34 @@ describe('play action', function() {
     assert.ok(radio.sendCommands.calledWith([
       ['clear'], ['add', 'track.mp3'], ['random', '0'], ['play', '0']
     ]));
+  });
+
+  it('sets play position', function() {
+    var radio = { sendCommands: sinon.spy() };
+
+    subject(radio, {
+      playlist: ['track0.mp3', 'track1.mp3'],
+      playNow: true, playPosition: '1'
+    });
+
+    assert.equal(1, radio.sendCommands.callCount);
+    assert.ok(radio.sendCommands.calledWith([
+      ['clear'], ['add', 'track0.mp3'], ['add', 'track1.mp3'], ['random', '0'],
+      ['play', '1']
+    ]));
+  });
+
+  it('rejects if position is larger that playlist', function(done) {
+    var radio = { sendCommands: sinon.spy() };
+
+    var promise = subject(radio, {
+      playlist: ['track.mp3'], playNow: true, playPosition: '3'
+    });
+
+    assert.isRejected(promise).then(function(){
+      assert.equal(0, radio.sendCommands.callCount);
+      done();
+    });
   });
 
   it('rejects if playlist is not empty', function(done){
