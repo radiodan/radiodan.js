f07c60613e7c5907e937fd821a61cc136d3f270e
Dan Nuttall <pixelblend@gmail.com>
2014-09-16 17:28:01 +0100

    Fix wait for socket test

--- test/lib/test-wait-for-socket.js
+++ test/lib/test-wait-for-socket.js
@@ -1,15 +1,6 @@
 var waitForSocket = require('../../lib/wait-for-socket');
 
 describe('waitForSocket', function(){
-  before(function() {
-    // chill winston
-    winston.remove(winston.transports.Console);
-  });
-
-  after(function() {
-    winston.add(winston.transports.Console);
-  });
-
   before(function () {
     this.port = 65535;
   });
@@ -43,10 +34,9 @@ describe('waitForSocket', function(){
       assert(this.mockSocket.setTimeout.calledWith(2500));
     });
 
-
     it('tries again on failure', function () {
       var clock = sinon.useFakeTimers(),
-          timeout = 10;
+          timeout = 1000;
 
       var waitFor = waitForSocket.create(this.port, this.mockSocket);
       var promise = waitFor.connect();
