d6283429027b098c406ecda1a25e06b2cba95dbf
Dan Nuttall <pixelblend@gmail.com>
2014-09-16 17:49:11 +0100

    Move log_level setting before logger is required

--- test/helper.js
+++ test/helper.js
@@ -1,10 +1,12 @@
 'use strict';
 
+//supress log messages
+process.env.LOG_LEVEL = 'fatal';
+
 var chai = require('chai'),
     cap  = require('chai-as-promised');
 
 global.sinon        = require('sinon');
-global.winston      = require('winston');
 global.fs           = require('fs');
 global.utils        = require('radiodan-client').utils;
 global.EventEmitter = require('events').EventEmitter;
@@ -12,5 +14,3 @@ global.assert       = chai.assert;
 
 chai.use(cap);
 
-//supress log messages
-process.env.LOG_LEVEL = 'fatal';
