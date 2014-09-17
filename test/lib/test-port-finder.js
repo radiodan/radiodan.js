4a8571fffdad6f1bef7f5697b3c254df0411e965
Dan Nuttall <pixelblend@gmail.com>
2014-09-17 15:27:29 +0100

    Pass a mocked port finder in test

--- test/lib/test-port-finder.js
+++ test/lib/test-port-finder.js
@@ -1,8 +1,18 @@
 var PortFinder = require('../../lib/port-finder');
 
 describe('PortFinder', function (){
+  beforeEach(function() {
+    var portFinder =  {};
+        portFinder.basePort = 49152;
+        portFinder.nextPortPromise = function() {
+          return utils.promise.resolve(portFinder.basePort++);
+        }
+
+   this.portFinder = portFinder;
+  });
+
   it('finds a single port', function(next) {
-    var subject   = PortFinder.create(),
+    var subject   = PortFinder.create(this.portFinder),
         nextPorts = subject.next(1);
 
     nextPorts.then(function(ports) {
@@ -11,16 +21,16 @@ describe('PortFinder', function (){
   });
 
   it('finds multiple ports', function(next) {
-    var subject   = PortFinder.create(),
+    var subject   = PortFinder.create(this.portFinder),
         nextPorts = subject.next(2);
 
     nextPorts.then(function(ports) {
-      assert.deepEqual([49152, 49153], ports);
+      return assert.deepEqual([49152, 49153], ports);
     }).then(next,next);
   });
 
   it('never finds the same port twice', function(next) {
-    var subject   = PortFinder.create(),
+    var subject   = PortFinder.create(this.portFinder),
         nextPorts = subject.next(1),
         morePorts = subject.next(1);
 
@@ -31,7 +41,7 @@ describe('PortFinder', function (){
   });
 
   it('finds no ports', function(next) {
-    var subject   = PortFinder.create(),
+    var subject   = PortFinder.create(this.portFinder),
         nextPorts = subject.next('lol');
 
     nextPorts.then(function(ports) {
