cc8b38daef40126c01a6fc6f42d9e59787b930f6
Dan Nuttall <pixelblend@gmail.com>
2014-09-24 11:55:32 +0100

    Add tests for state object

---
+++ test/lib/test-state.js
@@ -0,0 +1,19 @@
+var subject = require(libDir + 'state');
+
+describe('state', function() {
+  it('is an invoker', function() {
+    assert.isFunction(subject.invoke);
+  });
+
+  it('has a list of state-based methods', function() {
+    var methodNames = Object.keys(subject.methods);
+    assert.deepEqual(
+      methodNames,
+      ['database', 'player', 'playlist', 'volume']
+    );
+
+    methodNames.forEach(function(m) {
+      assert.isFunction(subject.methods[m]);
+    });
+  });
+});
