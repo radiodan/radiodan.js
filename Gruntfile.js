82b48b6f94a63803b35901c3542675ddc343a578
Chris Needham <chris.needham@bbc.co.uk>
2015-01-27 08:50:14 +0000

    Added jshint task to Grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -22,15 +23,42 @@ module.exports = function(grunt) {
         },
         files: ['test/**/test-*.js', 'lib/**/*.js'],
         tasks: ['default']
-     }
-   }
+      }
+    },
+    jshint: {
+      src: {
+        src: ['Gruntfile.js', 'lib/**/*.js', 'bin/*'],
+        options: {
+          node: true
+        }
+      },
+      test: {
+        src: ['test/**/*.js'],
+        options: {
+          node: true,
+          globals: {
+            it: true,
+            describe: true,
+            assert: true,
+            libDir: true,
+            sinon: true,
+            before: true,
+            beforeEach: true,
+            utils: true,
+            EventEmitter: true,
+            fs: true,
+            winston: true
+          }
+        }
+      }
+    }
   });
 
   // On watch events, if the changed file is a test file then configure mochaTest to only
   // run the tests from that file. Otherwise run all the tests
   var defaultTestSrc = grunt.config('mochaTest.test.src');
   grunt.event.on('watch', function(action, filepath) {
-  console.log(filepath);
+    console.log(filepath);
     grunt.config('mochaTest.test.src', defaultTestSrc);
     if (filepath.match('test/')) {
       grunt.config('mochaTest.test.src', filepath);
